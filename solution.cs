5da2951480d8c9b9a1809cc2d4ef3cc0bc62fcc4
Kooreis <kooreis.official@gmail.com>
2024-11-19 12:38:38 -0600

    This update introduces the BinaryTree class with a constructor that initializes the root of the tree to null.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,5 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+class BinaryTree {
+    constructor() {
+        this.root = null;
     }
 }
